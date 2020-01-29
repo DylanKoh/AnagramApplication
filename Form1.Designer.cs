81f5785c7e6ab93a63d6aa56d7090426aec8312b
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-29 09:21:47 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AnagramApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCharacter = new System.Windows.Forms.TextBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lbResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Characters: ";
+            // 
+            // txtCharacter
+            // 
+            this.txtCharacter.Location = new System.Drawing.Point(231, 46);
+            this.txtCharacter.Name = "txtCharacter";
+            this.txtCharacter.Size = new System.Drawing.Size(339, 22);
+            this.txtCharacter.TabIndex = 1;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(143, 100);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(89, 33);
+            this.btnRandom.TabIndex = 2;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(481, 100);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(89, 33);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // lbResults
+            // 
+            this.lbResults.FormattingEnabled = true;
+            this.lbResults.ItemHeight = 16;
+            this.lbResults.Location = new System.Drawing.Point(143, 158);
+            this.lbResults.Name = "lbResults";
+            this.lbResults.Size = new System.Drawing.Size(427, 260);
+            this.lbResults.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbResults);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.txtCharacter);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Anagram Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCharacter;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ListBox lbResults;
+    }
+}
+
