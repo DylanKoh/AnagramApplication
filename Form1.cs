c6ed084a730aeb7f651ceab0be9fb616a53d78d3
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-02-02 20:44:42 +0800

    Changed from Streamreader to ReadAllLines

--- Form1.cs
+++ Form1.cs
@@ -58,117 +58,114 @@ namespace AnagramApplication
         private void btnGenerate_Click(object sender, EventArgs e)
         {
             lbResults.Items.Clear();
-            using (var streamReader = new StreamReader("words_alpha.txt"))
+
+
+            var values = File.ReadAllLines("words_alpha.txt").ToList();
+            var array = txtCharacter.Text.ToLower().OrderBy(x => x).ToArray();
+
+            ///For every word in the dictionary....
+            foreach (var words in values)
             {
+                ///Breaks the word into a character array, then reorder them by alphabetical
+                ///order
+                var wordArray = words.OrderBy(x => x).ToArray();
 
-                var values = streamReader.ReadToEnd();
-                var stringSeperate = new string[] { "\r\n" };
-                var list = values.Split(stringSeperate, StringSplitOptions.None).ToList();
-                var array = txtCharacter.Text.ToLower().OrderBy(x => x).ToArray();
-
-                ///For every word in the dictionary....
-                foreach (var words in list)
+                ///Checks if the word in the dictionary is longer than the input
+                if (words.Length < txtCharacter.Text.Length)
                 {
-                    ///Breaks the word into a character array, then reorder them by alphabetical
-                    ///order
-                    var wordArray = words.OrderBy(x => x).ToArray();
+                    ///Initialise bool check to be true by default
+                    var boolCheck = true;
 
-                    ///Checks if the word in the dictionary is longer than the input
-                    if (words.Length < txtCharacter.Text.Length)
+                    ///Distinctify the characters of the current word in the dictionary
+                    var hashWord = wordArray.Distinct();
+
+                    foreach (var item in hashWord)
                     {
-                        ///Initialise bool check to be true by default
-                        var boolCheck = true;
-
-                        ///Distinctify the characters of the current word in the dictionary
-                        var hashWord = wordArray.Distinct();
-
-                        foreach (var item in hashWord)
+                        ///Checks for every new character for comparison, if bool check is false. If so, break the loop
+                        ///and skip to the next word in the dictionary
+                        if (!boolCheck) break;
+                        else
                         {
-                            ///Checks for every new character for comparison, if bool check is false. If so, break the loop
-                            ///and skip to the next word in the dictionary
-                            if (!boolCheck) break;
-                            else
+                            ///Checks if the input contains the word's character, else bool check will be false,
+                            ///breaking the loop and skipping the word in the dictionary
+                            if (!array.Contains(item))
                             {
-                                ///Checks if the input contains the word's character, else bool check will be false,
-                                ///breaking the loop and skipping the word in the dictionary
-                                if (!array.Contains(item))
-                                {
-                                    boolCheck = false;
-                                }
-                                else
-                                {
-                                    ///Checks if the current character, if it is more than the same character of the input,
-                                    ///change boolean check to be false, breaking the loop and not adding that specific word
-                                    if (array.Where(x => x == item).Select(x => x).Count() < wordArray.Where(x => x == item).Select(x => x).Count())
-                                    { 
-                                        boolCheck = false;
-                                    }
-                                    else
-                                    {
-                                        boolCheck = true;
-                                    }
-                                    
-                                }
-
-
+                                boolCheck = false;
                             }
-                        }
-
-                        ///If boolean check is still true after every check, add the word into the results list
-                        if (boolCheck)
-                        {
-                            lbResults.Items.Add(words);
-                        }
-
-                    }
-
-                    ///Checks if the word in the dictionary is as long as the input
-                    else if (words.Length == txtCharacter.Text.Length)
-                    {
-                        ///Initiate boolean check value
-                        var boolCheck = true;
-
-                        ///Index increase based on the length of the input character array
-                        for (int i = 0; i < array.Length; i++)
-                        {
-                            var item = array[i];
-
-                            ///Checks for every new character for comparison, if bool check is false. If so, break the loop
-                            ///and skip to the next word in the dictionary
-                            if (!boolCheck) break;
                             else
                             {
-                                ///Checks if the word's character and input character (since ordered by alphabetical order) matches.
-                                ///If it doesnt, change boolean check to be false, breaking the loop and not adding that specific word
-                                if (wordArray[i] != item)
+                                ///Checks if the current character, if it is more than the same character of the input,
+                                ///change boolean check to be false, breaking the loop and not adding that specific word
+                                if (array.Where(x => x == item).Select(x => x).Count() < wordArray.Where(x => x == item).Select(x => x).Count())
                                 {
                                     boolCheck = false;
                                 }
-
-                                ///Else remain true and eventually add the word into the results
                                 else
                                 {
                                     boolCheck = true;
                                 }
 
-
                             }
-                        }
 
-                        ///If boolean check is still true after every check, add the word into the results list
-                        if (boolCheck)
-                        {
-                            lbResults.Items.Add(words);
-                        }
 
+                        }
                     }
+
+                    ///If boolean check is still true after every check, add the word into the results list
+                    if (boolCheck)
+                    {
+                        lbResults.Items.Add(words);
+                    }
+
                 }
 
+                ///Checks if the word in the dictionary is as long as the input
+                else if (words.Length == txtCharacter.Text.Length)
+                {
+                    ///Initiate boolean check value
+                    var boolCheck = true;
+
+                    ///Index increase based on the length of the input character array
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        var item = array[i];
+
+                        ///Checks for every new character for comparison, if bool check is false. If so, break the loop
+                        ///and skip to the next word in the dictionary
+                        if (!boolCheck) break;
+                        else
+                        {
+                            ///Checks if the word's character and input character (since ordered by alphabetical order) matches.
+                            ///If it doesnt, change boolean check to be false, breaking the loop and not adding that specific word
+                            if (wordArray[i] != item)
+                            {
+                                boolCheck = false;
+                            }
+
+                            ///Else remain true and eventually add the word into the results
+                            else
+                            {
+                                boolCheck = true;
+                            }
 
 
+                        }
+                    }
 
+                    ///If boolean check is still true after every check, add the word into the results list
+                    if (boolCheck)
+                    {
+                        lbResults.Items.Add(words);
+                    }
 
+                }
             }
+
+
+
+
+
+
         }
     }
 }
